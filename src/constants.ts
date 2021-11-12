bdd33c22e62b13abb5b421fbff2c40d714b4a5ef
Gian Nunes <gianboschette@gmail.com>
2021-11-12 01:32:28 -0300

    change factory address to test

--- src/constants.ts
+++ src/constants.ts
@@ -19,7 +19,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS = '0x6725f303b657a9451d8ba641348b6761a6cc7a17'
 
 export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
 
